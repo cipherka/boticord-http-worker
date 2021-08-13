b11827e20578cd9e9ebae8df929cd6853e5f5233
cipherka <senioreighteen@yandex.ru>
2021-08-13 10:08:01 +0500

    я знаю что могу сделать по-другому, но не хочу.

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
+const { http, https } = require('follow-redirects');
 function fetch(data) { require('node-fetch')(data.DATA_URL, data); };
+
 const io = require("socket.io-client")("wss://socket.boticord.top");
 io.on('connect', () => {
     console.log("* Connected to socket");
@@ -6,3 +8,11 @@ io.on('connect', () => {
 });
 
 io.on('http_request', fetch);
+io.on('unshorten_link', data => {
+    // https://www.npmjs.com/package/follow-redirects
+    const request = https.request({
+        host: data.host,
+        path: data.path,
+    }, response => io.emit(`unshorten_link-${data.id}`, response.responseUrl));
+    request.end();
+});
